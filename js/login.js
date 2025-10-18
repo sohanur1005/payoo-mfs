b4c770064f06b911dcedc55917c95c24763712bf
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-18 23:52:22 +0600

    send this

--- js/login.js
+++ js/login.js
@@ -1,1 +1,8 @@
-console.log('abc');
+
+// set event handler
+document.getElementById('btn-login').addEventListener('click',function(event){
+    // set prevent default behaviour (prevent reloading browser)
+    event.preventDefault();
+
+
+})
