7ede20d6358bc84b513e19d3b31baa1d7608f175
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-20 10:28:50 +0600

    send this

--- js/login.js
+++ js/login.js
@@ -3,6 +3,15 @@
 document.getElementById('btn-login').addEventListener('click',function(event){
     // set prevent default behaviour (prevent reloading browser)
     event.preventDefault();
+    console.log('login button clicked')
+    // get the phone number
+    const mobileNumber=document.getElementById('mobile-number').value;
+    const pinNumber=document.getElementById('pin-number').value;
+
+    console.log(mobileNumber,pinNumber);
+
 
 
 })
+
+console.log('udhheu')
