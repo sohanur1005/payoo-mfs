c213b7159e726d97b636d2645d241e6c99e27905
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-23 18:59:20 +0600

    send this

--- js/login.js
+++ js/login.js
@@ -3,15 +3,25 @@
 document.getElementById('btn-login').addEventListener('click',function(event){
     // set prevent default behaviour (prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked')
+
+
     // get the phone number
     const mobileNumber=document.getElementById('mobile-number').value;
     const pinNumber=document.getElementById('pin-number').value;
 
     console.log(mobileNumber,pinNumber);
 
+    // bad  way to validate
+    if(mobileNumber == '5' && pinNumber =='123')
+    {
+        console.log('You are log in');
+    }
+    else{
+        alert('wrong number or pin');
+    }
+
 
 
 })
 
-console.log('udhheu')
+
