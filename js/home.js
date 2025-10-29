7c8e0afac018503b2dd295a03f120f16b000cf99
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-29 22:48:30 +0600

    send this

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log('abc')
+// step 1 add money to the account
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+    
+
+    // get money to be added  to the account
+    const addMoney=document.getElementById('input-add-money').value;
+    console.log(addMoney);
+
+    // get the pin provided
+    const pinNumber=document.getElementById('input-pin-number').value;
+    console.log(pinNumber)
+})
