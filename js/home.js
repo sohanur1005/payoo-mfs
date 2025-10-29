0fa9898be2c2851b84b9e7338dd4fbdcce7e3e6e
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-29 22:53:08 +0600

    send this

--- js/home.js
+++ js/home.js
@@ -11,4 +11,14 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // get the pin provided
     const pinNumber=document.getElementById('input-pin-number').value;
     console.log(pinNumber)
+
+    // verify pin number
+    if(pinNumber=='123')
+    {
+        console.log('Adding money to your account');
+
+    }
+    else{
+        alert('Wrong Pin! please try again')
+    }
 })
