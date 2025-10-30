850cf702566c9ece435968d7c06a10052be19427
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-30 23:45:10 +0600

    send this

--- js/add-money.js
+++ js/add-money.js
@@ -4,7 +4,25 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // get money and the pin number
     const addMoney=document.getElementById('input-add-money').value;
     const pinNumber=document.getElementById('input-pin-number').value;
-    console.log(addMoney,pinNumber)
+    
+
+    // verify pin
+    if(pinNumber == '123')
+    {
+        const balance=document.getElementById('account-balance').innerText;
+        const addMoneyNumber=parseFloat(addMoney);
+        const balanceNumber=parseFloat(balance);
+        // new Balance
+        const  newBalance= addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+        // update balance  with UI
+        document.getElementById('account-balance').innerText=newBalance;
+
+
+    }
+    else{
+        alert('Wrong Password')
+    }
 
 
 
