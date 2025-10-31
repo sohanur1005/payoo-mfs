2bed65f9fbe6c7e25ccf57147735b404fac15919
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-31 23:28:08 +0600

    send this

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,6 +1,10 @@
 document.getElementById('btn-cashout').addEventListener('click',function(event){
     event.preventDefault();
 
+    const cashOutMoney=document.getElementById('input-cash-out').value;
+    const pinNumber=document.getElementById('input-cashOut-pin-number').value;
+    console.log(cashOutMoney,pinNumber)
+
 
 
 
