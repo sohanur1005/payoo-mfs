0811f879a469e7f195df1381db0dc215ca4d2900
sohanur1005 <sohanur.sr1005@gmail.com>
2025-10-31 23:38:12 +0600

    send this

--- js/cashOut.js
+++ js/cashOut.js
@@ -3,7 +3,30 @@ document.getElementById('btn-cashout').addEventListener('click',function(event){
 
     const cashOutMoney=document.getElementById('input-cash-out').value;
     const pinNumber=document.getElementById('input-cashOut-pin-number').value;
-    console.log(cashOutMoney,pinNumber)
+    console.log(cashOutMoney,pinNumber);
+
+    if(pinNumber == '123')
+    {
+        const balance=document.getElementById('account-balance').innerText;
+        const cashOutBalance= parseFloat(cashOutMoney);
+         const balanceNumber=parseFloat(balance);
+
+         if(cashOutBalance>balance)
+         {
+            alert('Insufficient Balance')
+         }
+         else{
+            const newBalance=balanceNumber - cashOutBalance ;
+         console.log(newBalance)
+         document.getElementById('account-balance').innerText=newBalance;
+         }
+         
+
+    }
+    else
+    {
+        alert('Wrong Password');
+    }
 
 
 
