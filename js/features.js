4d97515a006e9a9dabd44b2670c09dbcfe64b2a2
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-08 23:18:15 +0600

    send this

--- js/features.js
+++ js/features.js
@@ -1,12 +1,15 @@
 document.getElementById('btn-show-cash-out').addEventListener('click',function(){
     document.getElementById('cash-out-form').classList.remove('hidden')
 
+    // hide add money
+
     document.getElementById('add-money-form').classList.add('hidden')
 
 
 })
 document.getElementById('btn-show-add-money').addEventListener('click',function(){
     document.getElementById('add-money-form').classList.remove('hidden')
+    // hide cash out
 
     document.getElementById('cash-out-form').classList.add('hidden')
 
